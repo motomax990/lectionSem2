ec150e3795b6c86b20730f68555ab2fa694ecb3c
motomax990 <motomax990@gmail.com>
2020-04-07 16:56:00 +0300

    Coздал пустые классы и главную функцию

---
+++ ContRab/contRab.py
@@ -0,0 +1,39 @@
+from random import randrange as rnd, choice
+import tkinter as tk
+import math
+import time 
+
+
+int main():
+    """
+    главня функция
+    
+    """
+    
+    pass
+
+
+
+class Target:
+    """
+    Класс мишени
+    
+    """
+    pass
+
+class Bullet:
+    """
+    Класс пули    
+    """
+    pass
+
+class Gun:
+    """
+    Класс оружия
+    
+    """    
+    pass
+
+
+if __name__ == "__main__":
+    main()
