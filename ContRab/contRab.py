740fa414e7bd5148218f435aed35dfd1f6bdbc18
motomax990 <motomax990@gmail.com>
2020-04-07 21:06:46 +0300

    Добавил названия и описания всех функций и классов

--- ContRab/contRab.py
+++ ContRab/contRab.py
@@ -4,36 +4,104 @@ import math
 import time 
 
 
-int main():
+def main():
     """
     главня функция
     
     """
-    
     pass
 
 
 
 class Target:
     """
-    Класс мишени
+        Класс мишени
     
     """
-    pass
-
+    def __init__(self):
+        pass
+    def new_target(self):
+        """
+            Создаёт новую мишень
+        """
+    def move(self):
+        """
+            Двигает мишень
+        """
+        pass
+    def set_set(self,x):
+        """
+            Установить состояние мишени
+        """
+        pass
+    def get_set(self):
+        """
+            Возвращает состояние мишени
+        """
+        pass
+    
+    
 class Bullet:
     """
-    Класс пули    
+        Класс пули    
     """
-    pass
-
+    def __init__(self):
+        pass
+    def new_bullet(self):
+        """
+            Создаёт новую пулю
+        """
+        pass
+    def move(self):
+        """
+            Двигает пулю
+        """
+    def delete_yourself(self):
+        """
+            Удаляет пулю
+        """
+    def check_hit(self):
+        """
+            Проверяет поподание в мишень
+        """
+    
+    
 class Gun:
     """
-    Класс оружия
+        Класс оружия
     
     """    
-    pass
+    def __init__(self):
+        pass
+
+    def new_gun(self):
+        """
+            Создаёт новый ствол
+        """
+    def targetting(self):
+        """
+            Производит прицеливание
+        """
+        pass
+    def strat_fire(self):
+        
+        """
+            Взводит флаг начала зарядки выстрела
+        """
+    def end_fire(self):
+        
+        """
+            Опускает флаг начала зарядки выстрела
+        """
+    def power_up(self):
+        
+        """
+            Увеличивает силу выстрела до возможного предела
+        """
+
 
 
 if __name__ == "__main__":
     main()
+else:
+    print("GO TO FAT ASS")
